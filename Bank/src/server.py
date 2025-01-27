c6b58631ca6ec75f17a439092521d8326f8a5e77
sharkpb8 <sharkpb8@gmail.com>
2025-01-27 12:48:25 +0100

    Update: ip,port from config

--- Bank/src/server.py
+++ Bank/src/server.py
@@ -1,17 +1,24 @@
 import socket
 import multiprocessing
 from src.client import client
+import json
 
 class server:
 
     def __init__(self):
-        server_inet_address = ("127.0.0.1", 65430)
+        ip = self.readconfig("ip")
+        if(self.is_invalid_ipv4(ip)):
+            ip = "127.0.0.1"
+        port = self.readconfig("port")
+        if(self.is_invalid_port(port)):
+            port = "65525"
+        server_inet_address = (ip, port)
         server_socket = socket.socket()
         server_socket.bind(server_inet_address)
         server_socket.listen()
 
         self.server_socket = server_socket
-        self.server_ip = server_inet_address[0]
+        self.server_ip = ip
 
         print("Server start on "+str(server_inet_address[0])+":"+str(server_inet_address[1]))
 
@@ -29,3 +36,36 @@ class server:
     def create_new_client(self,connection,server):
         c = client(connection,self.server_ip)
         c.run()
+
+    def readconfig(self,key):
+        with open("./Bank/config.json","r") as f:
+            config = json.load(f)
+            return config[key]
+        
+    def is_invalid_ipv4(self,ip):
+        parts = ip.split(".")
+        if (len(parts) != 4):
+            return True
+        try:
+            for part in parts:
+                num = int(part)
+                if num < 0 or num > 255:
+                    return True
+                if len(part) > 1 and part[0] == "0":
+                    return True
+        except ValueError:
+            return True
+        else:
+            return False
+        
+    def is_invalid_port(self,port):
+        try:
+            num = int(port)
+            if num < 65525 or num > 65535:
+                return True
+            if len(port) > 1 and port[0] == "0":    
+                return True
+        except ValueError:
+            return True
+        else:
+            return False
