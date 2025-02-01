c60d1d1b444764e5163a9423aedf6bf8bdfb0c2e
Sharkpb8 <sharkpb8@gmail.com>
2025-02-01 23:01:25 +0100

    Added: doc to Account

--- Bank/src/Account/Account.py
+++ Bank/src/Account/Account.py
@@ -2,11 +2,18 @@
 # from src.Error import *
 
 class Account:
+    """
+    A class representing a bank account.
+
+    Attributes
+    ----------
+    Account_number : int
+        The unique account number.
+    Balance : int
+        The current balance of the account.
+    """
     def __init__(self, Account_number, Balance):
 
         self.Account_number = int(Account_number)
 
         self.Balance = int(Balance)
-        
-    def __str__(self):
-        return (f"Číslo účtu: {self.Account_number}, zůstatek: {self.Balance}")
